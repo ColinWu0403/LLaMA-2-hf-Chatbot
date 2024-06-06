1d40595945969bf34c3c0ba2fde20651a6a98f89
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-06-06 15:24:52 +0800

    Updated Client pages

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -1,21 +1,55 @@
+import { useState } from "react";
+
 const ChatPage = () => {
+  const [message, setMessage] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handleSend = async () => {
+    try {
+      const res = await fetch("http://127.0.0.1:8000/chat/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message }),
+      });
+
+      const data = await res.json();
+      setResponse(data.message);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div className="flex flex-col h-screen mx-auto">
       <div className="px-20 py-14">
         <h1 className="text-4xl font-bold mb-4">Chat with Medical LLM</h1>
         <p className="text-lg mb-4">
-          Here, users can chat with the Medical LLM.
+          Ask questions about your health, get advice, and more.
+          <br />
+          The LLM is trained to specifically help you with your health concerns.
         </p>
 
-        <div className="w-full mx-auto">
+        <div className="w-full mx-auto flex flex-col items-center">
           <textarea
-            className="w-full bg-gray-100 border border-gray-300 rounded-lg p-4 mb-4 resize-none"
+            className="w-full max-w-3xl text-black bg-gray-100 border border-gray-300 rounded-lg p-4 mb-4 resize-none"
             rows="8"
             placeholder="Type your message here..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           ></textarea>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button
+            className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4 transition-all duration-150 ease-in"
+            onClick={handleSend}
+          >
             Send
           </button>
+          {response && (
+            <div className="w-full max-w-3xl text-black mt-4 p-4 bg-gray-100 border border-gray-300 rounded-lg break-words h-48 overflow-y-auto">
+              {response}
+            </div>
+          )}
         </div>
       </div>
     </div>
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,6 +3,8 @@ const HomePage = () => {
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold mb-4">Welcome to Revlis Chat</h1>
       <p className="text-lg mb-4">
+        Chinese Lorem Ipsum lol:
+        <br />
         巴朋登，鳥林子交合秋林根您乞笑色歌別歡抄錯，門尺見泉童可打真紅左服送跑玩甲馬示水亮，車記太月共弓中點欠荷說浪田視壯叫，空完蛋教流肖唱能杯急候沒童亭，民晚木。
         太洋給久誰封，哭現是上沒公時抄刃助布花斗布尼隻兌洋交送，村話回車民穴實爸干們貓禾畫又珠洋經語，旁玉言急南馬游胡服旁京哪，男兩跳巴荷假唱竹右斥天物即節蝸裝，平比入意不二給節民媽朵，幾尼花。
         直苦停植課。冬裝那尾言反南亭氣房太可，吉怕外聲別尼和四，怎掃民目用點「斥」風交蝶飯神頁雨哥。乾吉放彩，歡草且。
