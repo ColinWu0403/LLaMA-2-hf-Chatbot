a309a0ffc602575159865b8bea73a59dfdce7dfa
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-06-06 13:41:26 +0800

    Added TailwindCSS 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    plugins: [react()],
+  plugins: [react()],
 })
