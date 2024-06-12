9b91de64f1a016c1c54df41bfbb1bc5a69d26189
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-06-12 14:56:03 +0800

    Added papers/ directory

---
+++ papers/papersInfo.fsx
@@ -0,0 +1,2 @@
+// Inside contain subdirectories for each paper containining pdfs used to give the context of writing for the llm.
+// It is not added to github, you can use your own papers to fit your needs.
