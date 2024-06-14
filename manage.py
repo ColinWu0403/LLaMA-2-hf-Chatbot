4fde78233793f262c20e90f3ce831ff275b3c24b
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-06-14 10:25:25 +0800

    Added react app build script to runserver

--- manage.py
+++ manage.py
@@ -2,9 +2,20 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import subprocess
 
 
 def main():
+    # Run npm build command first
+    if os.name == 'nt':
+        npm_install = subprocess.Popen(["npm", "install"], cwd="client/", shell=True)
+        npm_build = subprocess.Popen(["npm", "run", "build"], cwd="client/", shell=True)
+    else:
+        npm_install = subprocess.Popen(["npm", "install"], cwd="client/")
+        npm_build = subprocess.Popen(["npm", "run", "build"], cwd="client/")
+    npm_install.wait()  # Wait for npm install to finish
+    npm_build.wait()  # Wait for npm build to finish
+    
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
     try:
