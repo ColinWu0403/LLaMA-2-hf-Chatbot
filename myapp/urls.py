e00adfc369ea2d4eeedfbe20246ebbda28b0387e
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-06-17 10:56:30 +0800

    Added 404 page redirection

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('chat/', views.chat_view, name='chat'),
+    re_path(r'^.*/$', TemplateView.as_view(template_name='index.html')),
+
 ]
