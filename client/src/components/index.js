75c8062466d756f68c7a9a349a913edb2963f9db
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-06-21 10:03:43 +0800

    Added hacker-text component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,5 @@
 import { EvervaultCard } from "./evervault-card";
 import { HeroHighlight } from "./hero-highlight";
 import { AuroraBackground } from "./aurora-background";
-export { EvervaultCard, HeroHighlight, AuroraBackground };
+import HackerText from "./hacker-text";
+export { EvervaultCard, HeroHighlight, AuroraBackground, HackerText };
